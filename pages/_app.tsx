37828f54ca0c662419537cc3f1442d519fd26eb5
Jason Liang <jasonliang512@gmail.com>
2023-01-26 17:52:13 -0800

    overlay fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -129,26 +129,28 @@ export default function App({ Component, pageProps }: AppProps) {
         }}
       >
         <div className={inter.className}>
-          <Transition
-            show={group.waitingFirstLoad}
-            className="fixed z-[9999] z-10 bg-gray-200 inset-0 p-8 flex items-end justify-end"
-            enter="transition ease-in-out duration-500 transform"
-            enterFrom="-translate-x-full opacity-0"
-            enterTo="translate-x-0 opacity-100"
-            leave="transition ease-in-out duration-500 transform"
-            leaveFrom="translate-x-0 opacity-100"
-            leaveTo="translate-x-full opacity-0"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 animate-spin text-gray-600">
-              <path fillRule="evenodd" d="M15.312 11.424a5.5 5.5 0 01-9.201 2.466l-.312-.311h2.433a.75.75 0 000-1.5H3.989a.75.75 0 00-.75.75v4.242a.75.75 0 001.5 0v-2.43l.31.31a7 7 0 0011.712-3.138.75.75 0 00-1.449-.39zm1.23-3.723a.75.75 0 00.219-.53V2.929a.75.75 0 00-1.5 0V5.36l-.31-.31A7 7 0 003.239 8.188a.75.75 0 101.448.389A5.5 5.5 0 0113.89 6.11l.311.31h-2.432a.75.75 0 000 1.5h4.243a.75.75 0 00.53-.219z" clipRule="evenodd" />
-            </svg>
-            <span className="text-gray-800 ml-2 font-semibold">
-              Loading...
-            </span>
-          </Transition>
-          <div className="min-h-screen">
-            <Nav />
-            <Component {...pageProps} />
+          <div className="relative">
+            <Transition
+              show={group.waitingFirstLoad}
+              className="fixed z-[9999] z-10 bg-gray-200 inset-0 p-8 flex items-end justify-end"
+              enter="transition ease-in-out duration-500 transform"
+              enterFrom="-translate-x-full opacity-0"
+              enterTo="translate-x-0 opacity-100"
+              leave="transition ease-in-out duration-500 transform"
+              leaveFrom="translate-x-0 opacity-100"
+              leaveTo="translate-x-full opacity-0"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 animate-spin text-gray-600">
+                <path fillRule="evenodd" d="M15.312 11.424a5.5 5.5 0 01-9.201 2.466l-.312-.311h2.433a.75.75 0 000-1.5H3.989a.75.75 0 00-.75.75v4.242a.75.75 0 001.5 0v-2.43l.31.31a7 7 0 0011.712-3.138.75.75 0 00-1.449-.39zm1.23-3.723a.75.75 0 00.219-.53V2.929a.75.75 0 00-1.5 0V5.36l-.31-.31A7 7 0 003.239 8.188a.75.75 0 101.448.389A5.5 5.5 0 0113.89 6.11l.311.31h-2.432a.75.75 0 000 1.5h4.243a.75.75 0 00.53-.219z" clipRule="evenodd" />
+              </svg>
+              <span className="text-gray-800 ml-2 font-semibold">
+                Loading...
+              </span>
+            </Transition>
+            <div className="z-0 relative min-h-screen">
+              <Nav />
+              <Component {...pageProps} />
+            </div>
           </div>
           <footer className="bg-gray-200 py-12 px-10 mt-12 flex flex-wrap gap-x-24 border-t border-gray-300">
             <div className="flex flex-col gap-y-3 mb-8">
